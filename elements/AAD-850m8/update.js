8e83705758be5b3200dca33fdde5e84f11581583
daishi <arakawa@smoo.co.jp>
2023-05-10 15:04:02 +0900

    properties

--- elements/AAD-850m8/update.js
+++ elements/AAD-850m8/update.js
@@ -7,11 +7,7 @@ function(instance, properties, context) {
   // 複数のマーカーの座標を配列に格納
   const markersData = [];
   if (properties.multiLocation) {
-    markersData = [
-      { lng: 140.11380313795797, lat: 36.08213568665823, imgUrl: 'https://picsum.photos/200' },
-      { lng: 140.112740, lat: 36.082176, imgUrl: 'https://picsum.photos/seed/picsum/200/300' },
-      { lng: 140.115064, lat: 36.081237, imgUrl: 'https://picsum.photos/200' }
-    ];
+    markersData = [properties.locations];
   }
 
   // 既存のマーカーを削除
@@ -23,48 +19,50 @@ function(instance, properties, context) {
   }
 
   // 複数のマーカーを地図に追加
-  markersData.forEach(markerData => {
-    const markerCoordinates = [markerData.lng, markerData.lat];
+  if (properties.locations) {
+    markersData.forEach(markerData => {
+      const markerCoordinates = [markerData.lng, markerData.lat];
 
-    const containerElement = document.createElement('div');
-    containerElement.style.marginTop = -properties.iconHeight + 'px';
+      const containerElement = document.createElement('div');
+      containerElement.style.marginTop = -properties.iconHeight + 'px';
 
-    // 画像アイコン用の要素を作成
-    let iconElement = new Image(properties.iconWidth, properties.iconHeight);
-    iconElement.src = markerData.imgUrl;
-    iconElement.style.borderRadius = '50%';
-    iconElement.style.border = '2px solid white';
-    iconElement.style.boxShadow = '0 2px 4px rgba(0, 0, 0, 0.3)';
+      // 画像アイコン用の要素を作成
+      let iconElement = new Image(properties.iconWidth, properties.iconHeight);
+      iconElement.src = markerData.imgUrl;
+      iconElement.style.borderRadius = '50%';
+      iconElement.style.border = '2px solid white';
+      iconElement.style.boxShadow = '0 2px 4px rgba(0, 0, 0, 0.3)';
 
-    // 吹き出しの三角形を表すSVG要素を作成
-    const triangleElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    triangleElement.setAttribute('width', '12');
-    triangleElement.setAttribute('height', '10');
-    triangleElement.setAttribute('viewBox', '0 0 12 10');
-    triangleElement.style.position = 'absolute';
-    triangleElement.style.top = 'calc(100% - 2px)';
-    triangleElement.style.left = 'calc(50% - 6px)';
-    triangleElement.style.transform = 'translateY(-50%)';
+      // 吹き出しの三角形を表すSVG要素を作成
+      const triangleElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+      triangleElement.setAttribute('width', '12');
+      triangleElement.setAttribute('height', '10');
+      triangleElement.setAttribute('viewBox', '0 0 12 10');
+      triangleElement.style.position = 'absolute';
+      triangleElement.style.top = 'calc(100% - 2px)';
+      triangleElement.style.left = 'calc(50% - 6px)';
+      triangleElement.style.transform = 'translateY(-50%)';
 
-    const pathElement = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    pathElement.setAttribute('d', 'M0,0 L6,10 L12,0 Z');
-    pathElement.setAttribute('fill', 'white');
+      const pathElement = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+      pathElement.setAttribute('d', 'M0,0 L6,10 L12,0 Z');
+      pathElement.setAttribute('fill', 'white');
 
-    triangleElement.appendChild(pathElement);
+      triangleElement.appendChild(pathElement);
 
-    // コンテナ要素にアイコンと三角形を追加
-    containerElement.appendChild(iconElement);
-    containerElement.appendChild(triangleElement);
+      // コンテナ要素にアイコンと三角形を追加
+      containerElement.appendChild(iconElement);
+      containerElement.appendChild(triangleElement);
 
-    const markerHeight = parseInt(iconElement.style.height) + parseInt(triangleElement.getAttribute('height'));
-    const markerOffset = [0, -markerHeight / 2];
+      const markerHeight = parseInt(iconElement.style.height) + parseInt(triangleElement.getAttribute('height'));
+      const markerOffset = [0, -markerHeight / 2];
 
-    const marker = new mapboxgl.Marker({ element: containerElement })
-      .setLngLat(markerCoordinates)
-      .addTo(instance.data.map);
+      const marker = new mapboxgl.Marker({ element: containerElement })
+        .setLngLat(markerCoordinates)
+        .addTo(instance.data.map);
 
-    instance.data.markers.push(marker);
-  });
+      instance.data.markers.push(marker);
+    });
+  }
 
   // mapオブジェクトの中心座標とズームレベルを更新
   if (instance.data.map) {
