b63168ac0f4a71dd33b2a0dad7c079aed21fb8cf
daishi <arakawa@smoo.co.jp>
2023-05-13 12:24:14 +0900

    up

--- elements/AAD-850m8/element_actions/AAd-850p8/run.js
+++ elements/AAD-850m8/element_actions/AAd-850p8/run.js
@@ -1,17 +1,17 @@
 function(instance, properties, context) {
 
-    if(instance.data.map) {
+  if (instance.data.map) {
 
-        // Get the current center coordinates
-        var center = instance.data.map.getCenter();
+    // Get the current center coordinates
+    var center = instance.data.map.getCenter();
 
-        // Log the coordinates to the console
-        console.log('Latitude: ' + center.lat + ', Longitude: ' + center.lng);
+    // Log the coordinates to the console
+    console.log('Latitude: ' + center.lat + ', Longitude: ' + center.lng);
 
-        // You can also return these values if you want to use them elsewhere
-        return {
-            lat: center.lat,
-            lng: center.lng
-        };
+    // You can also return these values if you want to use them elsewhere
+    return {
+      lat: center.lat,
+      lng: center.lng
     };
+  };
 }
