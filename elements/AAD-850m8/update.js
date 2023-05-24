d94be23f0bdb3f9dc1d6cd032a94fc08255c80c6
daishi <arakawa@smoo.co.jp>
2023-05-24 15:45:22 +0900

    fix

--- elements/AAD-850m8/update.js
+++ elements/AAD-850m8/update.js
@@ -225,7 +225,7 @@ function(instance, properties) {
 
   // クリックイベントを定義
   for (const marker of instance.data.markers) {
-    const markerId = marker.properties.image;
+    let markerId = marker.properties.id;
     marker.getElement().addEventListener('click', () => {
       if (!marker.getElement().classList.contains('active')) {
         marker.getElement().classList.add('active');
